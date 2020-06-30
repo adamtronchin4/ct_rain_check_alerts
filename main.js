500e3cb019150db38bb3b5a86a7d867d1040bc9d
adamtronchin4 <66394215+adamtronchin4@users.noreply.github.com>
2020-06-30 02:07:55 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const os = require('os')
-const net = require('net')
-
-console.log(os.arch())
-
-const server = net.createServer()
-const client = net.createConnection({port: 8124}, ()=>{
-    console.log("connected")
-})
